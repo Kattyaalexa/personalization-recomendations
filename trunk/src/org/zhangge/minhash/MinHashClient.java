63ad05e6ffcde8f8a95f7a29851e79468eed701c
zhgeaits@gmail.com <zhgeaits@gmail.com@a7e2eefd-92dc-6b01-ebdc-942ff38d8659>
2012-05-15 11:31:34 +0000

    提交主程序

---
+++ trunk/src/org/zhangge/minhash/MinHashClient.java
@@ -0,0 +1,39 @@
+package org.zhangge.minhash;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.mapreduce.Job;
+import org.zhangge.minhash.mapper.MinHashMapper;
+import org.zhangge.minhash.reducer.MinHashReducer;
+
+
+public class MinHashClient {
+
+	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+		Configuration conf = new Configuration();
+		conf = HBaseConfiguration.create(conf);
+		Job job = new Job(conf, "MinHashClient");
+		job.setJarByClass(MinHashClient.class);
+		//FileSystem fs = FileSystem.get(conf);
+		//fs.copyFromLocalFile(false, true, new Path(args[0]+"SeedValues"),new Path("/MinHash/SeedValues"));
+		Scan scan = new Scan();
+		scan.addFamily(Bytes.toBytes("story"));
+		TableMapReduceUtil.initTableMapperJob("UserTable",
+												scan,
+												MinHashMapper.class, 
+												ImmutableBytesWritable.class,
+												ImmutableBytesWritable.class,
+												job);
+		TableMapReduceUtil.initTableReducerJob("UserTable",
+												MinHashReducer.class,
+												job);
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		
+	}
+}
