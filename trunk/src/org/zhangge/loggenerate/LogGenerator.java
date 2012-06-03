f0c797e19603560ffdd7df74035d8f3ba80d1d07
zhgeaits@gmail.com <zhgeaits@gmail.com@a7e2eefd-92dc-6b01-ebdc-942ff38d8659>
2012-06-03 01:58:16 +0000

    读取movieline数据并写到hbase里

--- trunk/src/org/zhangge/loggenerate/LogGenerator.java
+++ trunk/src/org/zhangge/loggenerate/LogGenerator.java
@@ -1,9 +1,13 @@
 package org.zhangge.loggenerate;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Date;
+import java.util.ArrayList;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
@@ -15,11 +19,50 @@ import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.util.Bytes;
 
 public class LogGenerator {
+	
+	private ArrayList<Integer> uids = new ArrayList<Integer>();//用于存放用户id
+	private ArrayList<Integer> storyids = new ArrayList<Integer>();//用于存放story id
+	private ArrayList<Integer> scores = new ArrayList<Integer>();//用于存放打分
+	private ArrayList<Long> timestamp = new ArrayList<Long>();//用于存放时间
+	private double average;
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		//String path = "/home/zhangge/workspace/PersonalizationRecomendations/testdata/";
+		String filepath = "/home/zhangge/workspace/PersonalizationRecomendations/ml-100k/";
 		//new LogGenerator().generateToFile("newslog", path, "zhgeaits@gmail.com", "china.html", 10000);
-		new LogGenerator().generateToHbase("zhgeaits@gmail.com", "china.html", 10000);
+		LogGenerator lg = new LogGenerator();
+		lg.readData(filepath + "ua.base");
+		//new LogGenerator().generateToHbase("zhgeaits@gmail.com", "china.html", 10000);
+	}
+	
+	/**
+	 * 把Movieline的数据写入数据库
+	 * @param filepath
+	 */
+	public void readData(String filepath) {
+		try {
+			File file = new File(filepath);
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			int count = 0;//统计 总数
+			int sumScore = 0;//累加总分数
+			String line = null;
+			while((line = br.readLine()) != null) {
+				count ++;
+				String[] parts = line.split("\t");
+				sumScore += Integer.valueOf(parts[2]);
+				uids.add(Integer.valueOf(parts[0]));
+				storyids.add(Integer.valueOf(parts[1]));
+				scores.add(Integer.valueOf(parts[2]));
+				timestamp.add(Long.valueOf(parts[3]));
+			}
+			average = sumScore / count;
+			generateToHbase(count);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/**
@@ -30,7 +73,7 @@ public class LogGenerator {
 	 * @throws IOException
 	 * @throws InterruptedException 
 	 */
-	public void generateToHbase(String userId, String news, int count) throws IOException, InterruptedException {
+	public void generateToHbase(int count) throws IOException, InterruptedException {
 		Configuration config = HBaseConfiguration.create();
 		
 		HBaseAdmin admin = new HBaseAdmin(config);
@@ -39,7 +82,7 @@ public class LogGenerator {
 		admin.deleteTable(Bytes.toBytes("UserTable"));
 		
 		HTableDescriptor htd = new HTableDescriptor("UserTable");//一个表
-		HColumnDescriptor hcd1 = new HColumnDescriptor("clusters");//一个列族
+		HColumnDescriptor hcd1 = new HColumnDescriptor("clusters");//第一个列族
 		HColumnDescriptor hcd2 = new HColumnDescriptor("story");//第二个列族
 		htd.addFamily(hcd1);
 		htd.addFamily(hcd2);
@@ -51,14 +94,15 @@ public class LogGenerator {
 		}
 		
 		HTable table = new HTable(config, tablename);
-		byte[] row = Bytes.toBytes(userId);
-		Put put = new Put(row);
-		byte[] databytes = Bytes.toBytes("story");
 		for (int i = 0; i < count; i++) {
-			String col = news + i;
-			Date date = new Date();
-			put.add(databytes, Bytes.toBytes(col), Bytes.toBytes(String.valueOf(date.getTime())));
-			table.put(put);
+			if (scores.get(i) > average) {
+				byte[] row = Bytes.toBytes(uids.get(i).toString());
+				Put put = new Put(row);
+				byte[] family = Bytes.toBytes("story");
+				put.add(family, Bytes.toBytes(storyids.get(i).toString()), timestamp.get(i), Bytes.toBytes(timestamp.get(i).toString()));
+				//put.add(databytes, Bytes.toBytes(storyids.get(i).toString()), Bytes.toBytes(String.valueOf(date.getTime())));
+				table.put(put);
+			}
 		}
 	}
 	
